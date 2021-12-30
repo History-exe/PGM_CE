8e97bc9d99c86080b94565b80552937e09db7d0c
History_exe <33642965+History-exe@users.noreply.github.com>
2021-12-30 12:01:44 +0800

    PGM_CE src

---
+++ src/files/gallery/CGlist.lua
@@ -0,0 +1,13 @@
+CG_LIST = {
+	-- page1
+	{
+		-- 1st CGthumb
+		{"sv005a.png","sv005b.png","sv005c.png"},
+		-- 2nd CGthumb
+		{"sv006a.png","sv006b.png","sv006c.png"},
+	},
+	-- page2
+	{
+		
+	},
+}
